892dba6642c801a2f77ae4432ea713876ea02ab5
Kemal Gündoğdu <kemal.gundogdu20@ogr.atauni.edu.tr>
2025-03-12 22:22:09 +0300

    first theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
+import React from "react";
+// router
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+// pages
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <div className="max-w-[500px] mx-auto">
-        <div className="flex items-center justify-center h-5 w-full border-b border-gray-300">
-          <div className="h-5 w-5 bg-bilink-primary flex-1" />
-          <div className="h-5 w-5 bg-bilink-secondary flex-1" />
-          <div className="h-5 w-5 bg-bilink-background flex-1" />
-          <div className="h-5 w-5 bg-bilink-accent flex-1" />
-          <div className="h-5 w-5 bg-bilink-dark flex-1" />
-        </div>
-        <center className="text-3xl text-bilink-dark font-semibold mt-3">Bilink.</center>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
